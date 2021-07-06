aaef1561479135930c625c955215932d116c527a
guyu <1585824174@qq.com>
2021-07-06 09:30:58 +0800

    初始后端代码

---
+++ src/test/java/com/neutech/ShopServerDdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServerDdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
